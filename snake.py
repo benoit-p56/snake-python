55624faac3c2d31c84136dc33a2ceaa486a4654f
benoit-p56 <128241730+benoit-p56@users.noreply.github.com>
2024-03-21 19:33:48 -0400

    Initialized the snake object's state

--- snake.py
+++ snake.py
@@ -1,1 +1,9 @@
 import turtle as t
+
+class Snake():
+    snake = t.Turtle()
+    snake.shapesize(2, 2, 1)
+    snake.penup()
+    snake.shape("square")
+    snake.color("white")
+    snake.speed(1)
