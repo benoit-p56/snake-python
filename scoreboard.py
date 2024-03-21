70805c457b2b40ab838bae0222b83be1de3cac96
Juan <79994699+Juan-789@users.noreply.github.com>
2024-03-21 17:24:56 -0400

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,1 +1,23 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.goto(0, 250)
+        self.color('white')
+        self.hideturtle()
+        self.penup()
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.write(f'Score: {self.score}', False,
+                   'center', ('Arial', 15, 'normal'))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f'GAME OVER', False, 'center', ('Courier', 24, 'normal'))
 
