d4652f58bdb286ecb0697eabef5155f5a63c8a42
benoit-p56 <128241730+benoit-p56@users.noreply.github.com>
2024-04-03 19:10:59 -0400

    Created the gameloop and implemented wall collisions

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ import snake
 
 screen = t.Screen()
 screen.bgcolor("black")
+screen.setup(1000, 800)
 s = snake.Snake() 
+
+gaming = True
+
+x_border = (screen.window_width()/2) - 30
+y_border = (screen.window_height()/2) - 30
+
+while gaming:
+    if s.xcor() in [x_border, x_border*-1] or s.ycor() in [y_border, y_border*-1]:
+        gaming = False
+    s.forward(5)
+
 
 
 screen.exitonclick()
