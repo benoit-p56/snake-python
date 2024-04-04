7fc800d1a59651397a794a987ff5227972763494
benoit-p56 <128241730+benoit-p56@users.noreply.github.com>
2024-04-03 20:11:35 -0400

    Increased turtlesize and adjusted collisions accordingly
    Felt that the snake lacked presence on the screen

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ s = snake.Snake()
 
 gaming = True
 
-x_border = (screen.window_width()/2) - 30
-y_border = (screen.window_height()/2) - 30
+x_border = (screen.window_width()/2) - 40
+y_border = (screen.window_height()/2) - 35
+s.setheading(180)
 
 while gaming:
-    if s.xcor() in [x_border, x_border*-1] or s.ycor() in [y_border, y_border*-1]:
+    if s.xcor() in [x_border*-1 - 10, x_border] or s.ycor() in [ y_border*-1, y_border]:
         gaming = False
     s.forward(5)
 
--- snake.py
+++ snake.py
@@ -3,7 +3,7 @@ from turtle import Turtle
 class Snake(Turtle):
     def __init__(self, shape: str = "square") -> None:
         super().__init__(shape)
-        self.shapesize(2, 2, 1)
+        self.shapesize(3, 3, 1)
         self.penup()
         self.color("white")
         self.speed(1)
